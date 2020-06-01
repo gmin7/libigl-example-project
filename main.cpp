310598d29f9da905de7860f063f2613d08859b7c
Michelle <michelle.dimarco@mail.utoronto.ca>
2020-05-31 23:23:58 -0400

    Reimplemented the passthrough geometry shader

--- main.cpp
+++ main.cpp
@@ -27,6 +27,10 @@ int main(int argc, char *argv[])
       R"(
         
         #version 150
+        uniform mat4 view;
+        uniform mat4 proj;
+        uniform mat4 normal_matrix;
+
         in vec3 position;
         in vec3 normal;
         in vec2 texcoord;
@@ -34,18 +38,27 @@ int main(int argc, char *argv[])
         in vec4 Kd;
         in vec4 Ks;
 
-        out vec4 pos;
-
-        uniform mat4 view;
-        uniform mat4 proj;
-        uniform mat4 normal_matrix;
+        out VS_OUT {
+          vec3 position_eye;
+          vec3 normal_eye;
+          vec4 Ksi;
+          vec4 Kdi;
+          vec4 Kai;
+          vec2 texcoordi;
+        } vs_out;
 
         void main()
         {
-          gl_Position = proj * view * vec4(position, 1.0);
-          pos = proj * view * vec4(position, 1.0);
+          vs_out.position_eye = vec3 (view * vec4 (position, 1.0));
+          vs_out.normal_eye = vec3 (normal_matrix * vec4 (normal, 0.0));
+          vs_out.normal_eye = normalize(vs_out.normal_eye);
+          gl_Position = proj * vec4 (vs_out.position_eye, 1.0);
+
+          vs_out.Ksi = Ka;
+          vs_out.Kdi = Kd;
+          vs_out.Kai = Ks;
+          vs_out.texcoordi = texcoord;
         }
-      
       )";
 
     std::string mesh_geometry_shader_string =
@@ -54,17 +67,42 @@ int main(int argc, char *argv[])
         #version 150 core
 
         layout(triangles) in;
-        layout(triangle_strip, max_vertices = 3) out;
+        layout(triangle_strip, max_vertices = 5) out;
+
+        in VS_OUT {
+          vec3 position_eye;
+          vec3 normal_eye;
+          vec4 Ksi;
+          vec4 Kdi;
+          vec4 Kai;
+          vec2 texcoordi;
+        } gs_in[];
+
+        out VS_OUT {
+          vec3 position_eye;
+          vec3 normal_eye;
+          vec4 Ksi;
+          vec4 Kdi;
+          vec4 Kai;
+          vec2 texcoordi;
+        } gs_out;
 
         void main()
         {
-            gl_Position = gl_in[0].gl_Position;
-            EmitVertex();
-            gl_Position = gl_in[1].gl_Position;
-            EmitVertex();
-            gl_Position = gl_in[2].gl_Position;
-            EmitVertex();
-            EndPrimitive();
+          gs_out.position_eye = gs_in[0].position_eye;
+          gs_out.normal_eye   = gs_in[0].normal_eye;
+          gs_out.texcoordi    = gs_in[0].texcoordi;
+          gs_out.Kai          = gs_in[0].Kai;
+          gs_out.Kdi          = gs_in[0].Kdi;
+          gs_out.Ksi          = gs_in[0].Ksi;
+        
+          gl_Position = gl_in[1].gl_Position;
+          EmitVertex();
+          gl_Position = gl_in[2].gl_Position;
+          EmitVertex();
+          gl_Position = gl_in[0].gl_Position;
+          EmitVertex();
+          EndPrimitive();
         }
       
     )";
@@ -73,16 +111,58 @@ int main(int argc, char *argv[])
       R"(
         
         #version 150 
+
+        uniform vec4 fixed_color;
+        uniform mat4 view;
+        uniform mat4 proj;
+
+        in VS_OUT {
+          vec3 position_eye;
+          vec3 normal_eye;
+          vec4 Ksi;
+          vec4 Kdi;
+          vec4 Kai;
+          vec2 texcoordi;
+        } fs_out;
+
+        uniform vec3 light_position_eye;
+        vec3 Ls = vec3 (1, 1, 1);
+        vec3 Ld = vec3 (1, 1, 1);
+        vec3 La = vec3 (1, 1, 1);
+        uniform sampler2D tex;
+        uniform float specular_exponent;
+        uniform float lighting_factor;
+        uniform float texture_factor;
+
         out vec4 outColor;
 
         void main()
         {
-            outColor = vec4(0.0, 1.0, 0.0, 1.0);
+          vec3 Ia = 0.2 *La * vec3(fs_out.Kai);    // ambient intensity
+
+          vec3 vector_to_light_eye = light_position_eye - fs_out.position_eye;
+          vec3 direction_to_light_eye = normalize (vector_to_light_eye);
+          float dot_prod = dot (direction_to_light_eye, normalize(fs_out.normal_eye));
+          float clamped_dot_prod = max (dot_prod, 0.0);
+          vec3 Id = Ld * vec3(fs_out.Kdi) * clamped_dot_prod;    // Diffuse intensity
+
+          vec3 reflection_eye = reflect (-direction_to_light_eye, normalize(fs_out.normal_eye));
+          vec3 surface_to_viewer_eye = normalize (-fs_out.position_eye);
+          float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
+          dot_prod_specular = float(abs(dot_prod)==dot_prod) * max (dot_prod_specular, 0.0);
+          float specular_factor = pow (dot_prod_specular, specular_exponent);
+          vec3 Is = Ls * vec3(fs_out.Ksi) * specular_factor;    // specular intensity
+
+          vec4 color = vec4(lighting_factor * (Is + Id) + Ia + (1.0-lighting_factor) * 
+                       vec3(fs_out.Kdi),(fs_out.Kai.a+fs_out.Ksi.a+fs_out.Kdi.a)/3);
+          outColor = mix(vec4(1,1,1,1), texture(tex, fs_out.texcoordi), texture_factor) * color;
+          if (fixed_color != vec4(0.0)) outColor = fixed_color;
         }
     )";
 
     // std::string mesh_vertex_shader_string =
-    //   R"(#version 150
+    //   R"(
+    //   #version 150
     //   uniform mat4 view;
     //   uniform mat4 proj;
     //   uniform mat4 normal_matrix;
